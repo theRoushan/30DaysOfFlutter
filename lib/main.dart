f0b3e35df9b6f765520d26235a83faf354c04225
The Roushan <roushank00978@gmail.com>
2021-03-22 00:55:20 -0500

    Created Screens Folder to MAanage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tutorial/home.dart';
+import 'package:tutorial/Screens/home.dart';
 
 void main() => runApp(MyApp());
 
