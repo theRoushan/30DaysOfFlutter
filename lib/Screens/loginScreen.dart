3855000ea7fe3fc45d16d842aedc181a342e1b43
The Roushan <roushank00978@gmail.com>
2021-03-23 01:24:23 -0500

    Learned Container, Navigator class, Animated Container, Inkwell, learned to animate buttons animations using ternary operations

--- lib/Screens/loginScreen.dart
+++ lib/Screens/loginScreen.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial/Utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool onChanged = false;
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.white,
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 60, horizontal: 50),
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/Images/loginImage.png'),
-                  Text("Welcome Back!"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: "Username", hintText: "Enter Your Username"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: "Password", hintText: "Enter Password"),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  ElevatedButton(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 60, horizontal: 50),
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/Images/loginImage.png'),
+                Text("Welcome Back, $name"),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Username", hintText: "Enter Your Username"),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: "Password", hintText: "Enter Password"),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                /* ElevatedButton(
                     onPressed: () {
                       Navigator.pushNamed(context, AppRoutes.homeRoute);
                     },
@@ -37,11 +48,44 @@ class LoginPage extends StatelessWidget {
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.blue,
                         minimumSize: Size(150, 40)),
+                  ), */
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      onChanged = true;
+                    });
+                    Future.delayed(Duration(seconds: 1));
+                    Navigator.pushNamed(context, AppRoutes.homeRoute);
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    alignment: Alignment.center,
+                    height: 50,
+                    width: onChanged ? 50 : 150,
+                    child: onChanged
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(onChanged ? 50 : 10)),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
